7feea78e168c80ce4e4612567d5fa8ec9fbbe8fc
penguin138 <fridaythethirteens@gmail.com>
2016-05-06 16:22:41 +0300

    add naive implementation of svm loss

--- homework_3/linear_svm.py
+++ homework_3/linear_svm.py
@@ -2,7 +2,7 @@ import numpy as np
 # from random import shuffle
 
 
-def svm_loss_naive(W, X, y, reg):
+def svm_loss_naive(W, X, y, lambda_):
     """
     Structured SVM loss function, naive implementation (with loops)
     Inputs:
@@ -16,25 +16,37 @@ def svm_loss_naive(W, X, y, reg):
     - gradient with respect to weights W; an array of same shape as W
     """
     dW = np.zeros(W.shape)  # initialize the gradient as zero
-    loss = 0
-
-    ##########################################################################
-    # TODO:                                                                     #
-    # Compute the gradient of the loss function and store it dW.                #
-    # Rather that first computing the loss and then computing the derivative,   #
-    # it may be simpler to compute the derivative at the same time that the     #
-    # loss is being computed. As a result you may need to modify some of the    #
-    # code above to compute the gradient.                                       #
-    ##########################################################################
-
-    # Right now the loss is a sum over all training examples, but we want it
-    # to be an average instead so we divide by num_train.
-
-    # Add regularization to the loss.
+    data_loss = 0
+    all_scores = np.dot(W, X)
+    all_scores = all_scores.transpose()
+    X = X.transpose()
+    for i in range(X.shape[0]):
+        x = X[i]  # i-th element
+        scores = all_scores[i]
+        true_class = y[i]
+        true_class_score = scores[true_class]
+        current_loss = 0
+        for j in range(scores.shape[0]):
+            max_ = max(0, scores[j] - true_class_score + 1)
+            if max_ > 0:
+                if j != true_class:
+                    current_loss += max_
+                dW[j] += x
+                dW[true_class] -= x
+        data_loss += current_loss
+    data_loss /= X.shape[0]
+    regularization_loss = 0
+    for i in range(W.shape[0]):
+        for j in range(W.shape[1]):
+            regularization_loss += W[i][j] ** 2
+            dW[i][j] /= X.shape[0]
+            dW[i][j] += 2*lambda_*W[i][j]
+    regularization_loss *= lambda_
+    loss = data_loss + regularization_loss
     return loss, dW
 
 
-def svm_loss_vectorized(W, X, y, reg):
+def svm_loss_vectorized(W, X, y, lambda_):
     """
     Structured SVM loss function, vectorized implementation.
 
@@ -43,15 +55,12 @@ def svm_loss_vectorized(W, X, y, reg):
     loss = 0.0
     dW = np.zeros(W.shape)  # initialize the gradient as zero
 
-    ##########################################################################
-    # TODO:                                                                     #
-    # Implement a vectorized version of the structured SVM loss, storing the    #
-    # result in loss.                                                           #
-    ##########################################################################
-
-    ##########################################################################
-    #                             END OF YOUR CODE                              #
-    ##########################################################################
+    all_scores = np.dot(W, X)
+    all_scores = all_scores - all_scores[y, np.arange(all_scores.shape[1])] + 1
+    data_loss = np.sum(np.maximum(all_scores, 0)) - all_scores.shape[1]
+    data_loss /= all_scores.shape[1]
+    regularization_loss = lambda_ * np.sum(W ** 2)
+    loss = data_loss + regularization_loss
     ##########################################################################
     # TODO:                                                                     #
     # Implement a vectorized version of the gradient for the structured SVM     #
@@ -61,7 +70,9 @@ def svm_loss_vectorized(W, X, y, reg):
     # to reuse some of the intermediate values that you used to compute the     #
     # loss.                                                                     #
     ##########################################################################
-
+    
+    dW /= X.shape[0]
+    dW += 2*lambda_ * W
     ##########################################################################
     #                             END OF YOUR CODE                              #
     ##########################################################################
